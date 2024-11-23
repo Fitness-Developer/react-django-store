535e1d0b6e15daf76025ea879397dce686ade15c
Alexandr <mastertri97@gmail.com>
2024-11-23 11:18:06 +0300

    Initial commit

---
+++ back-python/backstore/items/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Items,Likes,Drawer,Orders
+
+
+admin.site.register(Items)
+admin.site.register(Likes)
+admin.site.register(Drawer)
+admin.site.register(Orders)
